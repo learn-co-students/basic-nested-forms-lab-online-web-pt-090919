22ed21a63b97bbd93de0d4766ab5a43fb2a3411f
jaredjames2020 <jaredjames2020@gmail.com>
2020-02-16 16:06:32 -0500

    recipe build name change

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(name: "first")
-    @recipe.ingredients.build(name: "second")
+    @recipe.ingredients.build(name: "1")
+    @recipe.ingredients.build(name: "2")
   end
 
   def create
